066eb827d79ffece32b91bf53f7683aa2bd1ac0b
AV365 <a.vodyanitsky@gmail.com>
2020-08-26 19:43:43 +0500

    4.2 fix: исправить замечания после ревью 4.1

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,22 @@
+let popup = document.querySelector('.popup');
+
+let editButton = document.querySelector('.button_edit');
+let closeButton = document.querySelector('.button_close');
+
+// Находим форму в DOM
+let formElement = document.querySelector('.form'); // Воспользуйтесь методом querySelector()
+
+let nameInput = document.querySelector('.form__profile-name');
+let jobInput = document.querySelector('.form__profile-job');
+
 function showPopup() {
-    let popup = document.querySelector('.popup');
+
     popup.classList.toggle('popup_opened');
 
-    let nameNow = document.querySelector('#profileName').textContent;
-    let jobNow = document.querySelector('#profileJob').textContent;
+    let nameNow = document.querySelector('.profile__person').textContent;
+    let jobNow = document.querySelector('.profile__job').textContent;
 
-    let nameInput = document.querySelector('#editProfileName');
-    let jobInput = document.querySelector('#editProfileJob');
+
     nameInput.value = nameNow;
     jobInput.value = jobNow;
 }
@@ -16,41 +26,37 @@ function showPopup() {
 // она никуда отправляться не будет
 function formSubmitHandler(evt) {
 
+// Получите значение полей из свойства value
+    let nameNew = document.querySelector('#editProfileName').value;
+    let jobNew = document.querySelector('#editProfileJob').value;
+
+// let nameNew = nameInput.value;
+// let jobNew = jobInput.value;
+
+// Выберите элементы, куда должны быть вставлены значения полей
+    let name = document.querySelector('.profile__person');
+    let job = document.querySelector('.profile__job');
+
+
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     // Так мы можем определить свою логику отправки.
     // О том, как это делать, расскажем позже.
     // Находим поля формы в DOM
 
-    // Получите значение полей из свойства value
-    let nameNew = document.querySelector('#editProfileName').value;
-    let jobNew = document.querySelector('#editProfileJob').value;
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let name = document.querySelector('#profileName');
-    let job = document.querySelector('#profileJob');
-
-
     // Вставьте новые значения с помощью textContent
+
     name.textContent = nameNew;
     job.textContent = jobNew;
 
 
-    let popup = document.querySelector('.popup');
     popup.classList.toggle('popup_opened');
 
 
 }
 
-editButton = document.querySelector('#editButton');
-closeButton = document.querySelector('#closeButton');
 
 editButton.addEventListener('click', showPopup);
 closeButton.addEventListener('click', showPopup);
-
-
-// Находим форму в DOM
-let formElement = document.querySelector('.form'); // Воспользуйтесь методом querySelector()
-
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
