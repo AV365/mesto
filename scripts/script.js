9c316d40b15530299b5f4cd3b666352736d81641
AV365 <a.vodyanitsky@gmail.com>
2020-09-13 15:03:25 +0500

    5.1 feature: ADD delete & insert place, preview place

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,47 @@
-let popup = document.querySelector('.popup');
+const initialCards = [
+    {
+        name: 'Алтай',
+        link: 'https://av365.github.io/mesto/images/photo1.jpg'
+    },
+    {
+        name: 'Крым',
+        link: 'https://av365.github.io/mesto/images/photo2.jpg'
+    },
+    {
+        name: 'Териберка',
+        link: 'https://av365.github.io/mesto/images/photo3.jpg'
+    },
+    {
+        name: 'Калининград',
+        link: 'https://av365.github.io/mesto/images/photo4.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://av365.github.io/mesto/images/photo5.jpg'
+    },
+    {
+        name: 'Башкирия',
+        link: 'https://av365.github.io/mesto/images/photo6.jpg'
+    },
 
-let editButton = document.querySelector('.button_edit');
-let closeButton = document.querySelector('.button_close');
+];
+
+const popupProfile = document.querySelector('.js-popup-profile');
+const popupCard = document.querySelector('.js-popup-card');
+const popupPlace = document.querySelector('.js-popup-place');
+
+const editButton = document.querySelector('.button_edit');
+
+const addCardButton = document.querySelector('.button_add');
 
 // Находим форму в DOM
-let formElement = document.querySelector('.form'); // Воспользуйтесь методом querySelector()
+const formElementProfile = document.querySelector('.js-form-profile');
+const formElementCard = document.querySelector('.js-form-card');
 
 let nameInput = document.querySelector('.form__profile-name');
 let jobInput = document.querySelector('.form__profile-job');
+
+let placeInput = document.querySelector('.form__card-name');
 
 let nameNow = document.querySelector('.profile__person');
 let jobNow = document.querySelector('.profile__job');
@@ -16,45 +50,153 @@ let jobNow = document.querySelector('.profile__job');
 let name = document.querySelector('.profile__person');
 let job = document.querySelector('.profile__job');
 
-function showPopup() {
+//Превью фото
+let imagePreview = document.querySelector('.preview__image');
+let titlePreview = document.querySelector('.preview__title');
 
-    popup.classList.toggle('popup_opened');
+const cardSection = document.querySelector('.cards');
+const cardTemplate = document.querySelector('.js-card-item-template').content;
+
+function nullImg(element) {
+
+    let replaceImg = element.closest('.card-item__pic');
+    replaceImg.attributes.src.value = './images/no-image.svg';
+
+}
+
+function showPopup(evt) {
+
+    if (evt.target.classList.contains('button_edit')) {
+
+        popupProfile.classList.toggle('popup_opened');
+        let closeButton = popupProfile.querySelector('.button_close');
+        closeButton.addEventListener('click', closePopup);
+
+        nameInput.value = nameNow.textContent;
+        jobInput.value = jobNow.textContent;
+
+    } else if (evt.target.classList.contains('button_add')) {
+
+        popupCard.classList.toggle('popup_opened');
+        placeInput.focus();
+        let closeButton = popupCard.querySelector('.button_close');
+        closeButton.addEventListener('click', closePopup);
+    } else if (evt.target.classList.contains('card-item__pic')) {
+        popupPlace.classList.toggle('popup_opened');
+
+        imagePreview.src = evt.target.attributes.src.value;
+        titlePreview.innerText = evt.target.attributes.alt.value;
+        imagePreview.alt = evt.target.attributes.alt.value;
+
+        let closeButton = popupPlace.querySelector('.button_close');
+        closeButton.addEventListener('click', closePopup);
+    }
 
 
-    nameInput.value = nameNow.textContent;
-    jobInput.value = jobNow.textContent;
+}
+
+function closePopup(evt) {
+    const currentPopup = evt.target.closest('section');
+    currentPopup.classList.toggle('popup_opened');
+
 }
 
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler(evt) {
+function formProfileSubmitHandler(evt) {
 
-// Получите значение полей из свойства value
-    let nameNew = document.querySelector('#editProfileName').value;
-    let jobNew = document.querySelector('#editProfileJob').value;
+    let nameNew = document.querySelector('.form__profile-name').value;
+    let jobNew = document.querySelector('.form__profile-job').value;
 
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.
-    // О том, как это делать, расскажем позже.
-    // Находим поля формы в DOM
-
-    // Вставьте новые значения с помощью textContent
-
+    evt.preventDefault();
     name.textContent = nameNew;
     job.textContent = jobNew;
 
-
-    popup.classList.toggle('popup_opened');
-
-
+    popupProfile.classList.toggle('popup_opened');
 }
 
 
+function formCardSubmitHandler(evt) {
+
+
+    let placeNew = document.querySelector('.form__card-name');
+    let urlNew = document.querySelector('.form__card-url');
+    evt.preventDefault();
+
+    let addPlace = [];
+    addPlace['name'] = placeNew.value;
+    addPlace['link'] = urlNew.value;
+
+    createCard(addPlace);
+    placeNew.value = '';
+    urlNew.value = '';
+
+    popupCard.classList.toggle('popup_opened');
+
+}
+
+// function checkImg(url) {
+//
+//     let tmpImg = document.createElement('img');
+//     let statusImg = false;
+//     tmpImg.src = url;
+//
+//     tmpImg.onerror = function() {statusImg = false;}
+//     tmpImg.onload = function()  {statusImg = true;}
+//
+//     return statusImg;
+// }
+
+
+function createCard(card) {
+
+
+    let cardNew = cardTemplate.cloneNode(true);
+
+    cardNew.querySelector('.card-item__title').innerText = card.name;
+    cardNew.querySelector('.card-item__pic').attributes.src.value = card.link;
+
+
+    cardNew.querySelector('.card-item__pic').attributes.alt.value = card.name;
+
+    let deleteButton = cardNew.querySelector('.js-card-delete');
+    deleteButton.addEventListener('click', deleteCard);
+
+    let likeButton = cardNew.querySelector('.js-card-like');
+    likeButton.addEventListener('click', likeCard);
+
+    let imgButton = cardNew.querySelector('.card-item__pic');
+    imgButton.addEventListener('click', showPopup);
+
+    cardSection.appendChild(cardNew);
+
+}
+
+function initCards(cards) {
+    cards.forEach(createCard);
+}
+
+function deleteCard(evt) {
+    let deleteCard = evt.target.parentNode;
+    deleteCard.classList.toggle('card-item_closed');
+
+    setTimeout(function () {
+        deleteCard.remove()
+    }, 150);
+
+}
+
+function likeCard(evt) {
+    let likeCard = evt.target;
+    likeCard.classList.toggle('button_like-isset');
+
+}
+
 editButton.addEventListener('click', showPopup);
-closeButton.addEventListener('click', showPopup);
+addCardButton.addEventListener('click', showPopup);
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
 
+formElementProfile.addEventListener('submit', formProfileSubmitHandler);
+formElementCard.addEventListener('submit', formCardSubmitHandler);
+
+
+initCards(initialCards);
