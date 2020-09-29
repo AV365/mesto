c17cfee67e412936a5efa642694607d304db7645
AV365 <a.vodyanitsky@gmail.com>
2020-09-29 09:20:45 +0500

    feat: Disable Save Button

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,7 +1,7 @@
 function enableValidation(settings) {
 // Определяем формы и инпуты
     const forms = document.querySelectorAll(settings['formSelector']);
-    const inputs = document.querySelectorAll(settings['inputSelector']);
+
 
 
     //отключаем сабмит
@@ -9,6 +9,7 @@ function enableValidation(settings) {
         formElement.addEventListener('submit', function (evt) {
             evt.preventDefault();
         });
+        setEventListeners(formElement, settings);
     });
 
     //Обрабатываем все инпуты
@@ -16,15 +17,28 @@ function enableValidation(settings) {
     //     inputElement.addEventListener('input', checkValidity(settings))
     // });
 
+
+}
+
+
+const setEventListeners = (formElement, settings) => {
+
+    const inputs = Array.from(formElement.querySelectorAll(settings['inputSelector']));
+    const saveBtn = formElement.querySelector(settings['submitButtonSelector']);
+    toggleButtonState(inputs, saveBtn, settings);
+
+    //console.log(saveBtn);
     inputs.forEach(inputElement => {
             inputElement.addEventListener('input', () => {
                 checkValidity(inputElement, settings);
-        })
+                toggleButtonState(inputs, saveBtn, settings);
+            })
         }
     );
 
-
 }
+
+
 
 function inputIsValid(inputElement) {
 
@@ -49,25 +63,36 @@ function checkValidity(inputElement, settings) {
 }
 
 
-function toggleButtonState(inputList, buttonElement) {
+function toggleButtonState(inputList, buttonElement, settings) {
+
+    //console.log(inputList);
+    console.log(buttonElement);
     if (hasInvalidInput(inputList)) {
-        buttonElement.classList.add('button_inactive');
+        buttonElement.classList.add(settings['inactiveButtonClass']);
     } else {
-        buttonElement.classList.remove('button_inactive');
+        buttonElement.classList.remove(settings['inactiveButtonClass']);
     }
+
+}
+
+function hasInvalidInput(inputList) {
+
+    return inputList.some((inputElement) => {
+        return !inputElement.validity.valid;
+    });
 
 }
 
 
 const showInputError = (formElement, settings) => {
 
-    console.log(formElement.validationMessage);
-    console.log(formElement.id);
-    console.log(settings);
+    //console.log(formElement.validationMessage);
+    //console.log(formElement.id);
+    //console.log(settings);
 
     inputElement = formElement.id;
     errorElement = document.querySelector(`#${inputElement}-error`);
-    console.log(errorElement);
+    //console.log(errorElement);
     errorElement.textContent = formElement.validationMessage;
 
     errorElement.classList.add(settings['errorClass']);
@@ -86,7 +111,7 @@ enableValidation({
     formSelector: '.form',
     inputSelector: '.form__input',
     submitButtonSelector: '.button_save',
-    inactiveButtonClass: 'popup__button_disabled',
+    inactiveButtonClass: 'button_disabled',
     inputErrorClass: 'popup__input_type_error',
     errorClass: 'form__error_active'
 });
